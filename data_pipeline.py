e4995ee14a5f6b268d3722ac46e561a8ed41ee39
asicoderOfficial <asicoderofficial@gmail.com>
2024-06-19 16:22:03 +0200

    Task 2: data extraction & storage pipeline, train & test. No split yet, just storage

---
+++ data_pipeline.py
@@ -0,0 +1,109 @@
+import os
+import logging
+from datetime import datetime
+
+import yaml
+from dotenv import load_dotenv
+import numpy as np
+from sentinelhub import (
+    CRS,
+    BBox,
+    bbox_to_dimensions,
+    SHConfig,
+)
+
+from src.data_acquisition.sentinel_hub_image_downloader import SentinelHubImageDownloader
+from src.data_acquisition.osm import OSM
+from src.data_acquisition.constants import EVALSCRIPTS
+from src.data_processing.utils import pytorch_convention_array, binarize
+from src.data_processing.patches import store_patches
+
+
+# Load configurations and credentials
+main_dir = os.getcwd()
+sentinel_config = SHConfig()
+with open(f'{main_dir}/config/data_pipeline.yml', 'r') as file:
+    data_pipeline_config = yaml.load(file, Loader=yaml.FullLoader)
+load_dotenv(f'{main_dir}/config/.env.local')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+sentinel_config.sh_client_id = os.getenv("SH_CLIENT_ID")
+sentinel_config.sh_client_secret = os.getenv("SH_CLIENT_SECRET")
+
+if not sentinel_config.sh_client_id or not sentinel_config.sh_client_secret:
+    logging.warning(f"Warning! To use Process API, please provide the credentials (OAuth client ID and client secret). They must be located in the {main_dir}/config/.env.local file, and be named SH_CLIENT_ID and SH_CLIENT_SECRET.")
+    exit()
+
+# Set parameters from the yml file
+resolution = data_pipeline_config['sentinel']['resolution']
+max_cloud_coverage = data_pipeline_config['sentinel']['max_cloud_coverage']
+evalscript = EVALSCRIPTS[data_pipeline_config['sentinel']['evalscript']]
+cities = data_pipeline_config['cities']
+patches_side_sizes = data_pipeline_config['patches']['sizes']
+patches_stride = data_pipeline_config['patches']['stride']
+patches_save_dir = data_pipeline_config['patches']['save_dir']
+verbose = data_pipeline_config['verbose']
+
+if not os.path.exists(patches_save_dir):
+    os.makedirs(patches_save_dir)
+if not os.path.exists(f'{patches_save_dir}/original_images'):
+    os.makedirs(f'{patches_save_dir}/original_images')
+
+# Get the corresponding coordinates bounding boxes for each of the specified cities
+osm = OSM(imgs_tmp_folder='tmp', imgs_tmp_extension='png')
+for city in cities:
+    city_name = city['name']
+    # Bounding boxes
+    north = city['north']
+    south = city['south']
+    east = city['east']
+    west = city['west']
+    if verbose:
+        logging.info(f"Downloading data for city {city['name']} with coordinates, north: {city['north']}, south: {city['south']}, east: {city['east']}, west: {city['west']}")
+    osmnx_bbox = (north, south, east, west)
+    sentinel_bbox = BBox(bbox=(west, south, east, north), crs=CRS.WGS84)
+
+    size = bbox_to_dimensions(sentinel_bbox, resolution=resolution)
+    if size[0] > 2500 or size[1] > 2500:
+        logging.warning(f"Warning! The size of the image is too big ({size[0]}x{size[1]} pixels) for city {city}. The maximum size allowed is 2500x2500 pixels for SentinelHub API in free version.")
+        exit()
+    # Date range
+    end_date = datetime.now()
+    start_date = end_date.replace(month=end_date.month-1)
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+
+    # Download the Sentinel-2A image data with the specified bands (RGB or multispectral)
+    downloader = SentinelHubImageDownloader(sentinel_bbox, size, sentinel_config, max_cloud_coverage=max_cloud_coverage)
+    downloader.time_interval = (start_date, end_date)
+    downloader.evalscript = evalscript
+    city_data = downloader.download_image()
+    city_data = city_data[0]
+    if verbose:
+        logging.info(f"Downloaded the city data for {city_name} from SentinelHub with shape {city_data.shape}")
+
+    # Download the buildings data from OpenStreetMaps (target labels)
+    buildings_data = osm.buildings(osmnx_bbox, dimensions=size)
+    if verbose:
+        logging.info(f"Downloaded the buildings data for {city_name} from OpenStreetMaps with shape {buildings_data.shape}")
+
+    # Reshape them to have PyTorch like convention (C, H, W), instead of (W, H, C)
+    city_data = pytorch_convention_array(city_data)
+    buildings_data = pytorch_convention_array(buildings_data)
+    buildings_data = binarize(buildings_data)
+
+    # Save the original images, so that if later different patches are needed, they can be extracted from the original images, avoiding a new download
+    city_path = f'{patches_save_dir}/original_images/{city_name}_city.npy'
+    buildings_path = f'{patches_save_dir}/original_images/{city_name}_buildings.npy'
+    np.save(city_path, city_data)
+    np.save(buildings_path, buildings_data)
+    if verbose:
+        logging.info(f"Saved the original images for {city_name} in {city_path} and {buildings_path}")
+
+    # Extract patches from the images and save them
+    if verbose:
+        logging.info(f"Extracting patches from the images for {city_name}")
+    store_patches(city_data, buildings_data, patches_save_dir, city_name, side_sizes=patches_side_sizes, stride=patches_stride)
+    if verbose:
+        logging.info(f"Finished extracting patches from the images for {city_name}. Find them in {patches_save_dir}/, each city has a subdirectory, and all patches are merged together in a single file, called buildings.npy or city.npy")
