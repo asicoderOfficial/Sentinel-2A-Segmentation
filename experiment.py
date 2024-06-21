de34ff8bd49d8cf5955cc78bbc006b32c10c8ade
asicoderOfficial <asicoderofficial@gmail.com>
2024-06-21 16:50:37 +0200

    Task 3: experiments yml file to configure and launch fast

--- experiment.py
+++ experiment.py
@@ -1,73 +1,106 @@
-import torch
+import os
 import traceback
+import logging
+from datetime import datetime
+
+import torch
+import yaml
 from sklearn.utils import check_random_state
 
 from src.ml.dataset import SentinelDataset
-from src.ml.models.unet import UNet
-from src.ml.constants import RANDOM_SEED
+from src.ml.constants import RANDOM_SEED, DEFAULT_CONFIG_PATH, MODELS_DECODER, LOSS_FN_DECODER, OPTIMIZER_DECODER
 from src.ml.run import Run
-from src.ml.logs.logger import Logger
 
-# Constants
+
+# Parse configuration from yml file
+config_path = input(f'Please enter the path to the configuration YAML file (default: {DEFAULT_CONFIG_PATH}): ')
+if config_path == '':
+    config_path = DEFAULT_CONFIG_PATH
+
+with open(config_path, 'r') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+logging_dir = config['logging']['dir']
+if not os.path.exists(logging_dir):
+    os.makedirs(logging_dir)
+train_data_dir = config['train']['data_dir']
+
 # Set a fixed random seed for reproducibility
-torch.manual_seed(RANDOM_SEED)
-check_random_state(RANDOM_SEED)
+seed = config['seed'] if 'seed' in config else RANDOM_SEED
+torch.manual_seed(seed)
+check_random_state(seed)
 
-in_channels = 13
-out_channels = 1
+# Set the device to use
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if config['device'] == 'auto' else torch.device(config['device'])
+# Model
+model_name = config['model']['name'].lower()
+model_parameters = config['model']['parameters']
+# Loss function
+criterion_name = config['loss']['name'].lower()
+criterion_parameters = config['loss']['parameters']
+# Optimizer
+optimizer_name = config['optimizer']['name'].lower()
+optimizer_parameters = config['optimizer']['parameters']
+# Model training hyperparameters
+train_percentages = config['train']['mode']['perc']
+batch_sizes = config['train']['hyperparameters']['batch_size']
+epochs = config['train']['hyperparameters']['epochs']
+learning_rates = config['train']['hyperparameters']['lr']
+early_stopping = config['train']['early_stopping']
+# Data for train-val and for test
+train_val_dataset = SentinelDataset(train_data_dir)
+#test_dataset = SentinelDataset(config['test']['data_dir'])
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-# Hyperparameters
-train_percentages = [0.7, 0.8, 0.9]
-batch_sizes = [128, 16, 32, 64]
-epochs = [15, 40, 100]
-learning_rates = [0.001, 0.01, 0.1]
-early_stopping = {'delta': 0.001, 'patience': 4}
-
-# Data
-dataset = SentinelDataset('data/Berlin')
-
-
+verbose = config['verbose']
 
 for curr_train_percentage in train_percentages:
     for curr_batch_size in batch_sizes:
         for curr_epoch in epochs:
             for curr_learning_rate in learning_rates:
                 try:
-                    print(f'Running experiment with train percentage {curr_train_percentage}, batch size {curr_batch_size}, epoch {curr_epoch} and learning rate {curr_learning_rate}.')
-                    experiment_id = f'UNet_trperc_{curr_train_percentage}_batch_{curr_batch_size}_epoch_{curr_epoch}_lr_{curr_learning_rate}'
+                    curr_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+                    experiment_id = f'{curr_time}_{model_name}_trperc_{curr_train_percentage}_batch_{curr_batch_size}_epoch_{curr_epoch}_lr_{curr_learning_rate}'
+                    # Set up logging
+                    if not os.path.exists(f"runs/{experiment_id}/{logging_dir}/"):
+                        os.makedirs(f"runs/{experiment_id}/{logging_dir}/")
+                    log_file = f"runs/{experiment_id}/{logging_dir}/training_logs.log"
+                    logging.basicConfig(level=logging.INFO,
+                                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                        handlers=[
+                                            logging.FileHandler(log_file),
+                                            logging.StreamHandler()
+                                        ])
+                    logger = logging.getLogger(__name__)
+                    if verbose:
+                        logger.info(f'Running experiment with train percentage {curr_train_percentage}, batch size {curr_batch_size}, epoch {curr_epoch} and learning rate {curr_learning_rate}.')
                     # Model hyperparameters
                     model_hyperparameters = {'batch_size': curr_batch_size, 'learning_rate': curr_learning_rate}
                     # Trainer hyperparameters
                     trainer_hyperparameters = {'epochs': curr_epoch, 'early_stopping': early_stopping}
 
                     # Model
-                    model = UNet(in_channels=in_channels, out_channels=out_channels)
+                    model = MODELS_DECODER[model_name](**model_parameters)
 
                     # Loss function
-                    criterion = torch.nn.BCEWithLogitsLoss()
+                    criterion = LOSS_FN_DECODER[criterion_name](**criterion_parameters)
 
                     # Optimizer
-                    optimizer = torch.optim.Adam(model.parameters(), lr=curr_learning_rate)
-
-                    r = Run(id=experiment_id, train_dataset=dataset, model=model, criterion=criterion, optimizer=optimizer,
-                            train_percentage=curr_train_percentage, trainer_hyperparameters=trainer_hyperparameters, model_hyperparameters=model_hyperparameters,
-                            device=device, verbose=True)
+                    optimizer = OPTIMIZER_DECODER[optimizer_name](model.parameters(), **optimizer_parameters)
 
                     # Execute the experiment! Cross your fingers!
+                    r = Run(id=experiment_id, train_dataset=train_val_dataset, model=model, criterion=criterion, optimizer=optimizer,
+                            train_percentage=curr_train_percentage, trainer_hyperparameters=trainer_hyperparameters, model_hyperparameters=model_hyperparameters,
+                            device=device, verbose=verbose, logger=logger)
                     r.run()
+                    exit()
 
-                # Allow keyboard interrupts such as CTRL+C to stop the execution
                 except KeyboardInterrupt:
-                    print()
-                    print(f'Experiment {experiment_id} interrupted.')
+                    # Allow keyboard interrupts such as CTRL+C to stop the execution
+                    logger.warning(f'Experiment {experiment_id} interrupted via keyboard. Exiting.')
                     # Leave the program
                     exit()
                 except:
-                    print(f'Experiment {experiment_id} failed.')
-                    print(traceback.format_exc())
+                    logger.error(f'Experiment {experiment_id} failed. Error:')
+                    logger.error(traceback.format_exc())
+                    # Continue trying the next experiment
                     exit()
-                    Logger.error(run_info_dir=r.run_info_dir, error=traceback.format_exc())
-                    continue
