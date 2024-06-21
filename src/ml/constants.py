f75be83243f2958227385b846b9219edded837d6
asicoderOfficial <asicoderofficial@gmail.com>
2024-06-21 16:51:05 +0200

    Task 3: parse yml model/optimizer/loss constants

--- src/ml/constants.py
+++ src/ml/constants.py
@@ -1,8 +1,32 @@
+from src.ml.models.baseline import Baseline
+from src.ml.models.unet import UNet
+import torch
+
 # Training
 EXPERIMENTS_SAVE_DIR = 'runs'
 RANDOM_SEED = 42
+DEFAULT_CONFIG_PATH = 'config/experiments.yml'
 
 # Data
 NPY_DATA_DIR = 'data/'
 N_CLASSES = 4
 LABELS = [0, 1]
+
+# Parsing yml configuration
+# Models
+MODELS_DECODER = {
+    'baseline': Baseline,
+    'unet': UNet
+}
+
+# Loss functions
+LOSS_FN_DECODER = {
+    'bcewithlogits': torch.nn.BCEWithLogitsLoss
+}
+
+# Optimizers
+OPTIMIZER_DECODER = {
+    'adam': torch.optim.Adam,
+    'sgd': torch.optim.SGD
+}
+
