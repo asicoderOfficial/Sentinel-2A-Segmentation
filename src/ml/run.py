284074011803c42005220b7b4f6efd6b52bf5c14
asicoderOfficial <asicoderofficial@gmail.com>
2024-07-05 03:13:45 +0200

    Task 3: integrate splits of train/val data

--- src/ml/run.py
+++ src/ml/run.py
@@ -6,8 +6,9 @@ import torch.nn as nn
 from torch.utils.data import random_split
 from torch.utils.data.dataloader import DataLoader
 
-from src.ml.dataset import SentinelDataset
-from src.ml.constants import EXPERIMENTS_SAVE_DIR, RANDOM_SEED
+from src.ml.data.dataset import SentinelDataset
+from src.ml.data.split import random_split_by_percentage, density_split
+from src.ml.constants import EXPERIMENTS_SAVE_DIR
 from src.ml.trainer import Trainer
 from src.ml.logs.logger import Logger
 from src.ml.logs.plots import TrainingPlots
@@ -17,7 +18,7 @@ from src.ml.logs.plots import TrainingPlots
 class Run:
     def __init__(self, id: str, train_dataset: SentinelDataset, model: nn.Module, criterion, optimizer: torch, 
                  train_percentage: float=0.7, trainer_hyperparameters: dict={'epochs': 20}, model_hyperparameters: dict={'batch_size': 16, 'learning_rate': 0.001},
-                 device:torch.device=torch.device('cuda'), test_dataset: SentinelDataset=None, verbose: bool=False, logger:Logger=None) -> None:
+                 device:torch.device=torch.device('cuda'), test_dataset: SentinelDataset=None, split_mode:str='density', verbose: bool=False, logger:Logger=None) -> None:
         """ To run an experiment.
 
         Args:
@@ -31,6 +32,7 @@ class Run:
             model_hyperparameters (dict, optional): The model hyperparameters. Defaults to {'batch_size': 16, 'learning_rate': 0.001}.
             device (torch.device, optional): The device to use. Defaults to torch.device('cuda').
             test_dataset (SentinelDataset, optional): The testing dataset (for inference, no labels). Defaults to None.
+            split_mode (str, optional): How to split the data. Defaults to 'density'. Modes: 'density' or 'random'.
             verbose (bool, optional): Whether to print information of the run. Defaults to False.
             logger (Logger, optional): The logger to use. Defaults to None.
 
@@ -52,7 +54,7 @@ class Run:
 
         self.run_info_dir = f'{EXPERIMENTS_SAVE_DIR}/{self.id}'
 
-        self._dataloaders()
+        self._dataloaders(split_mode)
         self._directories()
 
         if self.device.type != 'cuda' and verbose:
@@ -140,15 +142,21 @@ class Run:
             )
 
 
-    def _dataloaders(self) -> None:
+    def _dataloaders(self, split_mode:str='density') -> None:
         """ Creates the dataloaders.
+
+        Args:
+            split_mode (str, optional): The mode to split the data. Defaults to 'density'. Modes:
+                - density: splits the data by the density of the buildings, having a balanced distribution of buildings in the train and validation sets.
+                - random: splits the data randomly, without any consideration of the distribution of the buildings, just the percentage of the train set.
 
         Returns:
             None
         """        
-        train_size = int(self.train_percentage * len(self.train_dataset))
-        val_size = len(self.train_dataset) - train_size
-        train_dataset, val_dataset = random_split(self.train_dataset, [train_size, val_size], generator=torch.Generator().manual_seed(RANDOM_SEED))
+        if split_mode == 'density':
+            train_dataset, val_dataset = density_split(self.train_dataset, self.train_percentage)            
+        else:
+            train_dataset, val_dataset = random_split_by_percentage(self.train_dataset, self.train_percentage)
 
         self.train_loader = DataLoader(train_dataset, batch_size=self.model_hyperparameters['batch_size'], shuffle=True)
         self.val_loader = DataLoader(val_dataset, batch_size=self.model_hyperparameters['batch_size'], shuffle=False)
