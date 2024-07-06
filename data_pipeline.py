5eefb785e20b5a644819b29420cd2694430a1e8f
asicoderOfficial <asicoderofficial@gmail.com>
2024-07-06 19:27:55 +0200

    Task 2: fix & refactor merge functionality

--- data_pipeline.py
+++ data_pipeline.py
@@ -16,7 +16,7 @@ from sentinelhub import (
 from src.data_acquisition.sentinel_hub_image_downloader import SentinelHubImageDownloader
 from src.data_acquisition.osm import OSM
 from src.data_acquisition.constants import EVALSCRIPTS
-from src.data_processing.utils import pytorch_convention_array, binarize
+from src.data_processing.utils import pytorch_convention_array, binarize, merge_patches
 from src.data_processing.patches import store_patches
 
 
@@ -138,30 +138,8 @@ if download:
 
 if merge:
     # Merge all downloaded patches together into the same npy files. One for the cities and one for the buildings, for each patch side size
-    all_cities_patches = {}
-    all_buildings_patches = {}
-    for city in cities:
-        city_name = city['name']
-        data_set = city['set']
-        patches_save_dir_set = f'{patches_save_dir}/{data_set}'
-        npy_files = [f for f in os.listdir(f'{patches_save_dir_set}/{city_name}')]
-        city_files = [f for f in npy_files if 'city' in f]
-        buildings_files = [f for f in npy_files if 'buildings' in f]
-        for city_file in city_files:
-            patch_size = city_file.split('_')[1].split('.')[0]
-            if patch_size not in all_cities_patches:
-                all_cities_patches[city_file.split('_')[1].split('.')[0]] = []
-            city_patches = np.load(f'{patches_save_dir_set}/{city_name}/{city_file}', mmap_mode='r')
-            all_cities_patches[patch_size].append(city_patches)
-        for buildings_file in buildings_files:
-            patch_size = buildings_file.split('_')[1].split('.')[0]
-            if patch_size not in all_buildings_patches:
-                all_buildings_patches[buildings_file.split('_')[1].split('.')[0]] = []
-            buildings_patches = np.load(f'{patches_save_dir_set}/{city_name}/{buildings_file}', mmap_mode='r')
-            all_buildings_patches[patch_size].append(buildings_patches)
-
-    for patch_size, city_patches in all_cities_patches.items():
-        np.save(f'{patches_save_dir}/city_{patch_size}.npy', np.concatenate(city_patches))
-    for patch_size, building_patches in all_buildings_patches.items():
-        np.save(f'{patches_save_dir}/buildings_{patch_size}.npy', np.concatenate(building_patches))
+    train_cities = [city for city in cities if city['set'] == 'train']
+    test_cities = [city for city in cities if city['set'] == 'test']
     
+    merge_patches(train_cities, patches_save_dir)
+    merge_patches(test_cities, patches_save_dir)
