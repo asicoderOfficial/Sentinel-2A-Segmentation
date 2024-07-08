f5be98e8ee1318db8ab4c6068226f1386e0a7273
sultan numyalai <sn@go2-markets.com>
2024-07-08 19:47:57 +0200

    [ fix]: changing input channels to in channels

--- src/ml/models/baseline.py
+++ src/ml/models/baseline.py
@@ -1,16 +1,16 @@
 import torch.nn as nn
 
 class Baseline(nn.Module):
-    def __init__(self, input_channels: int):
+    def __init__(self, in_channels: int, out_channels: int=1):
         super(Baseline, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(input_channels, 32, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 1, kernel_size=1, padding=0)
+            nn.Conv2d(128, out_channels, kernel_size=1, padding=0)
         )
 
     def forward(self, x):
